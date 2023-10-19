8e2ad431b7ae3be30d579bde216f320bf2d1875f
Tommy-Dan <ttuckerwra@gmail.com>
2023-10-19 15:25:57 -0700

    modified file

--- monty.h
+++ monty.h
@@ -40,11 +40,6 @@ typedef struct instruction_s
         void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-typedef struct instruction_s
-{
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
-} instruction_t;
 
 
 /*file operations*/
